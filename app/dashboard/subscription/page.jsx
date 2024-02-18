00f73c4668b095653f23ac30928d64ccc77f1bbe
Peter Wambua <pwambua25@gmail.com>
2024-02-18 10:23:38 +0300

    subscription

--- app/dashboard/subscription/page.jsx
+++ app/dashboard/subscription/page.jsx
@@ -1,4 +1,17 @@
 const Subscription = () => {
+
+    async function initiatePayment() {
+        let options = {
+            method: 'POST',
+            body: {
+                'amount' : 100,
+                'name' : 'peter',
+                'email': 'peter@mail.com'
+            }
+        };
+        let initPay = await fetch('http://localhost:3000/api/intasend', options);
+    }
+
     return (  
         <div>
             Thank you, your payment is being processed hang on!
