3f92319844c1095928e47d58f6918a57bf4f76d3
Peter Wambua <pwambua25@gmail.com>
2024-02-19 20:17:40 +0300

    open ai api consumption

--- app/api/open-ai/route.js
+++ app/api/open-ai/route.js
@@ -1,116 +1,143 @@
-// export default function handler(req, res) {
-//   const {method} = req;
-
-//    let result = process.env
-//    res.status(200).json({ result })
-// }
-import fs, { existsSync } from 'fs';
-import url from 'url';
-//import fetch from 'node-fetch';
-import fetch from 'node-fetch';
-
-// "https://firebasestorage.googleapis.com/v0/b/ai-app-49d1e.appspot.com/o/users%2FCURRICULUM%20VITAE%20(CV)-%20Peter%20Wambua.docx?alt=media&token=3295b8bf-0f72-4164-bd0f-e230039f8d3b"
-
-export async function GET(req, res) {
-   
-   const url = 'https://api.edenai.run/v2/ocr/resume_parser';
-   const options = {
-      method: 'POST',
-      headers: {accept: 'application/json', 'content-type': 'application/json', 'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`},
-      body: JSON.stringify({
-         response_as_dict: true,
-         attributes_as_list: false,
-         show_original_response: false,
-         convert_to_pdf: false,
-         providers: 'affinda,hireability',
-         file_url: "https://firebasestorage.googleapis.com/v0/b/ai-app-49d1e.appspot.com/o/user%2FVRmkj5XwI5gJ1fWxm5UKSAgHsHJ3?alt=media&token=5d9810ad-e457-4e6d-9fa9-8b88f5107b84"
-      })
-   };
-
-   let d = await fetch(url, options);
-   let reszz = await d.json();
-   console.log(reszz);
-   return new Response(reszz);
-   
-}
-
+import OpenAI from "openai";
 
 export async function POST(req, res) {
-   // const url = 'https://api.edenai.run/v2/ocr/resume_parser';
-   // const options = {
-   //    method: 'POST',
-   //    headers: {accept: 'application/json', 'content-type': 'application/json', 'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`},
-   //    body: JSON.stringify({
-   //       response_as_dict: true,
-   //       attributes_as_list: false,
-   //       show_original_response: false,
-   //       convert_to_pdf: false,
-   //       providers: 'affinda,hireability',
-   //       file_url: "https://firebasestorage.googleapis.com/v0/b/ai-app-49d1e.appspot.com/o/user%2FVRmkj5XwI5gJ1fWxm5UKSAgHsHJ3?alt=media&token=5d9810ad-e457-4e6d-9fa9-8b88f5107b84"
-   //    })
-   // };
+   // get user job desc and skills
+   let reqBody = await req.json();
+   let { jobDescription, skills } = reqBody;
 
-   // let d = await fetch(url, options);
-   // let reszz = await d.json();
-   // console.log(reszz);\
-   const body  = await req.json();
-   console.log(body['data']);
-   return new Response(body['data']);
+   if (!jobDescription || jobDescription == null) {
+      return Response('job description not included',{status: 400})
+   }
+
+   if (!skills || skills == null) {
+      return Response('skills not included',{status: 400})
+   }
+
+   const openAi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+   try {
+      const completion = await openAi.chat.completions.create({
+         model: 'gpt-3.5-turbo',
+         messages: [
+            {role: "system", content: "You are going to generate resume content from now on"},
+            {role: 'user', content: `make me an about me for a resume i am applying jo description is ${jobDescription} and here are my skills ${skills}` }
+         ]
+      });
+   
+      return new Response(completion.choices[0].message.content, {status: 200});
+   } catch (error) {
+      return new Response('error occurred please try again',{status: 500})
+   }
    
 }
 
 
 
+// import fs, { existsSync } from 'fs';
+// import url from 'url';
+// //import fetch from 'node-fetch';
 
+// // "https://firebasestorage.googleapis.com/v0/b/ai-app-49d1e.appspot.com/o/users%2FCURRICULUM%20VITAE%20(CV)-%20Peter%20Wambua.docx?alt=media&token=3295b8bf-0f72-4164-bd0f-e230039f8d3b"
 
-// export async function GET(req, res) {
-//    //C:\Users\pwamb\Documents\next-js-projects\resume-builder
-//    console.log(process.cwd());
-//    let file = process.cwd() + '\\app\\images\\resume.docx';
-//    // fs.readFile(file, (d, md) => {
-//    //    console.log(md);
-//    // });
-//    const data = new FormData();
-//    data.append('providers','affinda');
-//    data.append('file',fs.createReadStream(file));
+// export async function POST(req, res) {
    
-//     let options = {
-//         method: 'POST',
-//         headers: {
-//             'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`,
-//             'Content-Type':  `multipart/form-data; boundary=${data}`
-//         },
-//         body: data,
-//     };
-//    let response = await fetch('https://api.edenai.run/v2/ocr/resume_parser', options);
-//    let result = process.env;
-//    let text = await response.text();
-//    console.log(response.status);
-//    return new Response(text);
-//    res.status(200).json({ 'peter':'peter' });
-// }
-
-// export async function GET() {
-//    let file = process.cwd() + '\\app\\images\\resume.docx';
-//    let mUrl = url.pathToFileURL(file);
-//    const uri = 'https://api.edenai.run/v2/ocr/resume_parser';
+//    const url = 'https://api.edenai.run/v2/ocr/resume_parser';
 //    const options = {
-//    method: 'POST',
-//    headers: {accept: 'application/json', 'content-type': 'application/json', 'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`,},
-//    body: JSON.stringify({
-//       response_as_dict: true,
-//       attributes_as_list: false,
-//       show_original_response: false,
-//       convert_to_pdf: false,
-//       providers: 'senseloaf,affinda,klippa,hireability',
-//       file_url: fs.createReadStream(file)
-//    })
+//       method: 'POST',
+//       headers: {accept: 'application/json', 'content-type': 'application/json', 'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`},
+//       body: JSON.stringify({
+//          response_as_dict: true,
+//          attributes_as_list: false,
+//          show_original_response: false,
+//          convert_to_pdf: false,
+//          providers: 'affinda,hireability',
+//          file_url: "https://firebasestorage.googleapis.com/v0/b/ai-app-49d1e.appspot.com/o/user%2FVRmkj5XwI5gJ1fWxm5UKSAgHsHJ3?alt=media&token=5d9810ad-e457-4e6d-9fa9-8b88f5107b84"
+//       })
 //    };
 
-//    fetch(uri, options)
-//    .then(res => res.json())
-//    .then(json => console.log(json))
-//    .catch(err => console.error(err));
-//    return new Response(mUrl);
-
+//    let d = await fetch(url, options);
+//    let reszz = await d.json();
+//    console.log(reszz);
+//    return new Response(reszz);
+   
 // }
+
+
+// export async function POST(req, res) {
+//    // const url = 'https://api.edenai.run/v2/ocr/resume_parser';
+//    // const options = {
+//    //    method: 'POST',
+//    //    headers: {accept: 'application/json', 'content-type': 'application/json', 'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`},
+//    //    body: JSON.stringify({
+//    //       response_as_dict: true,
+//    //       attributes_as_list: false,
+//    //       show_original_response: false,
+//    //       convert_to_pdf: false,
+//    //       providers: 'affinda,hireability',
+//    //       file_url: "https://firebasestorage.googleapis.com/v0/b/ai-app-49d1e.appspot.com/o/user%2FVRmkj5XwI5gJ1fWxm5UKSAgHsHJ3?alt=media&token=5d9810ad-e457-4e6d-9fa9-8b88f5107b84"
+//    //    })
+//    // };
+
+//    // let d = await fetch(url, options);
+//    // let reszz = await d.json();
+//    // console.log(reszz);\
+//    const body  = await req.json();
+//    console.log(body['data']);
+//    return new Response(body['data']);
+   
+// }
+
+
+
+
+
+// // export async function GET(req, res) {
+// //    //C:\Users\pwamb\Documents\next-js-projects\resume-builder
+// //    console.log(process.cwd());
+// //    let file = process.cwd() + '\\app\\images\\resume.docx';
+// //    // fs.readFile(file, (d, md) => {
+// //    //    console.log(md);
+// //    // });
+// //    const data = new FormData();
+// //    data.append('providers','affinda');
+// //    data.append('file',fs.createReadStream(file));
+   
+// //     let options = {
+// //         method: 'POST',
+// //         headers: {
+// //             'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`,
+// //             'Content-Type':  `multipart/form-data; boundary=${data}`
+// //         },
+// //         body: data,
+// //     };
+// //    let response = await fetch('https://api.edenai.run/v2/ocr/resume_parser', options);
+// //    let result = process.env;
+// //    let text = await response.text();
+// //    console.log(response.status);
+// //    return new Response(text);
+// //    res.status(200).json({ 'peter':'peter' });
+// // }
+
+// // export async function GET() {
+// //    let file = process.cwd() + '\\app\\images\\resume.docx';
+// //    let mUrl = url.pathToFileURL(file);
+// //    const uri = 'https://api.edenai.run/v2/ocr/resume_parser';
+// //    const options = {
+// //    method: 'POST',
+// //    headers: {accept: 'application/json', 'content-type': 'application/json', 'Authorization': `Bearer ${process.env.NEXT_EDEN_AI_KEY}`,},
+// //    body: JSON.stringify({
+// //       response_as_dict: true,
+// //       attributes_as_list: false,
+// //       show_original_response: false,
+// //       convert_to_pdf: false,
+// //       providers: 'senseloaf,affinda,klippa,hireability',
+// //       file_url: fs.createReadStream(file)
+// //    })
+// //    };
+
+// //    fetch(uri, options)
+// //    .then(res => res.json())
+// //    .then(json => console.log(json))
+// //    .catch(err => console.error(err));
+// //    return new Response(mUrl);
+
+// // }
--- app/dashboard/resume-create/page.jsx
+++ app/dashboard/resume-create/page.jsx
@@ -2,9 +2,26 @@
 import { useEffect, useState } from 'react';
 import ResumeAi from '../openiai/page';
 import { Input } from 'react-daisyui';
+import ProfileDetails from '../cv-create/proceed/templates/add-edit/profile';
+import AboutAddEdit from '../cv-create/proceed/templates/add-edit/about';
+import ExperienceAddEdit from '../cv-create/proceed/templates/add-edit/experience';
+import EducationAddEdit from '../cv-create/proceed/templates/add-edit/education';
+import SkillAddEdit from '../cv-create/proceed/templates/add-edit/skills';
+import AwardAddEdit from '../cv-create/proceed/templates/add-edit/awards';
+import ReferencesEditDelete from '../cv-create/proceed/templates/add-edit/references';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/app/firebase/firebase';
+import HobbiesAddEdit from '../cv-create/proceed/templates/add-edit/hobbies';
+import Languages from '../cv-create/proceed/templates/add-edit/languages';
+import LinksUser from '../cv-create/proceed/templates/add-edit/links';
+import ProjectsAddEdit from '../cv-create/proceed/templates/add-edit/projects';
+import Image from 'next/image';
+import resumeImage from '@/app/images/peter2.png';
 
 const CreateResume = () => {
     const [showJobDescriptionInput, setShowJobDescriptionInput] = useState(true);
+    const [firebase_user, loading, error] = useAuthState(auth);
+
     // const [summary, setSummary] = useState(null);
 
     // async function getSummary() {
@@ -16,17 +33,39 @@ const CreateResume = () => {
     //     getSummary();
     // }, [])
 
-    return (  
-        <div>
-            {showJobDescriptionInput ? 
-            <div className='p-10'>
-                <Input type='text' placeholder='Enter job description here...' />
-            </div> 
-            : 
-            <div></div>
-            }
+    return (
+        <div className="md:grid md:grid-cols-4 bg-slate-200">
+            <div className="bg-white pt-10 pl-5 pr-5">
+                <ProfileDetails userId={firebase_user.uid} />
+                <AboutAddEdit useId={firebase_user.uid} />
+                <ExperienceAddEdit user_id={firebase_user.uid} />
+                <EducationAddEdit userId={firebase_user.uid} />
+                <SkillAddEdit user_id={firebase_user.uid} />
+                <AwardAddEdit userId={firebase_user.uid} />
+                <ReferencesEditDelete userId={firebase_user.uid} />
+                <HobbiesAddEdit userId={firebase_user.uid} />
+                <Languages userId={firebase_user.uid} />
+                <LinksUser userId={firebase_user.uid} />
+                <ProjectsAddEdit userId={firebase_user.uid} />
+            </div>
+            <div className="md:col-span-3 p-10">
+                {showJobDescriptionInput ?
+                    // job desc input
+                    <div className='pl-8 pr-8  flex flex-col gap-10 items-center'>
+                        <Image src={resumeImage} alt='ai-resume' width={120} height={120} className='w-[40%] h-[40%]' />
+                        <div className='w-full text-center'>
+                            <p className='font-semibold mb-4'>Welcome, spark up your resume</p>
+                            <Input type='text' className='w-full bg-white border-amber-500' placeholder='Enter job description here...' />
+                        </div>
+
+                    </div>
+                    :
+                    // resume
+                    <div></div>
+                }
+            </div>
         </div>
     );
 }
- 
+
 export default CreateResume;
